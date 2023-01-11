e5efa4984c9f78677aeb445502d0dd717d36ab76
Yurii Oborskiy <yuriioborskiy@gmail.com>
2023-01-11 23:56:26 +0200

    fix: update state using prevstate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,15 +17,15 @@ export default function App() {
   const feedbackButtonClick = opt => {
     switch (opt) {
       case 'good':
-        setGood(good + 1);
+        setGood(good => good + 1);
         break;
 
       case 'neutral':
-        setNeutral(neutral + 1);
+        setNeutral(neutral => neutral + 1);
         break;
 
       case 'bad':
-        setBad(bad + 1);
+        setBad(bad => bad + 1);
         break;
       default:
         return;
